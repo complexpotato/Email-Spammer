a8e0f6d80ac5370a8a1e28ac848ee93e185d516a
Justin Chang <33967054+Juniorn1003@users.noreply.github.com>
2018-01-24 15:23:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-apt-get update -y
-apt-get autoremove
-sudo apt-get install pip
-pip install pysocks
+apt-get update -y          # Update
+apt-get autoremove         # Auto Remove
+
+sudo "apt-get install pip" # Installing Pip
+echo installed pip         # Telling User that installing is done
+
+pip install pysocks        # Installing pysocks
+echo "installed pysocks"   # Teling User that installing is done
+
+chmod +x Email-Spam.py     # Giving permission to Email-Spam.py
